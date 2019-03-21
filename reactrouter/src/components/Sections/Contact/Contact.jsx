eba59be8281244348bf860f81598ad254d7040e0
Christian Peralta <christianipc.1987@gmail.com>
2019-03-21 17:41:30 -0300

    REACTROUTER, le doy estilos a las distintas secciones

--- reactrouter/src/components/Sections/Contact/Contact.jsx
+++ reactrouter/src/components/Sections/Contact/Contact.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import Navigation from '../../Navigation/Navigation';
-
+import './Contact.css'
 
 const Contact = ()=>(
 	<React.Fragment>
-		<Navigation />
-		<h2>Contact</h2>
+	<Navigation />
+	<div class="card card-image card-image__contact">
+		<div class="text-white text-center py-5 px-4 my-5">
+			<div>
+				<h2 class="card-title h1-responsive pt-3 mb-5 font-bold"><strong>Contact</strong></h2>
+			</div>
+		</div>
+	</div>
 	</React.Fragment>
 )
 
